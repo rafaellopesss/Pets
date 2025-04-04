2e9b9a76978e87ac56cf235fa465de1e89537f7f
Rafael L. <118671591+rafaellopesss@users.noreply.github.com>
2025-04-04 10:51:52 -0300

    atualizar

--- cards/javascript.js
+++ cards/javascript.js
@@ -1,16 +1,34 @@
 
 let criarElemento = () => {
-    debugger
-    let guardaCards = document.getElementById('dad')
+    let nomeAnimal = document.getElementById('nomeAnimal').value
+    let descricaoAnimal = document.getElementById('descricaoAnimal').value
+    
+    let guardaCards = document.getElementById('dad') // Div pai que guarda todos os cards 
 
-    let criarCard = document.createElement('div')
+    let criarCard = document.createElement('div') // Criei a div de cada card 
     criarCard.setAttribute('class', 'card')
 
-    let criarImagem = document.createElement('img')
-    criarImagem.src = {src: 'caramela2.webp'}
-    
+    let criarImagem = document.createElement('img') //Criei a tag img 
+    criarImagem.src = 'caramela2.webp'
     criarImagem.setAttribute('class', 'imagemdoanimal')
     
     guardaCards.appendChild(criarCard)
+    criarCard.appendChild(criarImagem)
+
+    let sectionDescricao = document.createElement('section')
+    sectionDescricao.setAttribute('class', 'descricao')
     
+    let titulodescricao = document.createElement('h1')
+    titulodescricao.setAttribute('class', 'titulo')
+    titulodescricao.innerText = nomeAnimal
+
+    let paragrafodescricao = document.createElement('p')
+    paragrafodescricao.setAttribute('class', 'sobreoanimal')
+    paragrafodescricao.innerText = descricaoAnimal
+
+    criarCard.appendChild(sectionDescricao)
+    localStorage.setItem('')
+    sectionDescricao.appendChild(titulodescricao)
+    sectionDescricao.appendChild(paragrafodescricao)
+
 }
