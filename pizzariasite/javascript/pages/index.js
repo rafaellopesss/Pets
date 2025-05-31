d75ab4ddc8ec52678266dfed799013c7766082a1
Rafael L. <118671591+rafaellopesss@users.noreply.github.com>
2025-05-31 09:01:21 -0300

    Efeito carousel

--- pizzariasite/javascript/pages/index.js
+++ pizzariasite/javascript/pages/index.js
@@ -1,8 +1,13 @@
 let card = document.getElementsByClassName('card')
 let buttonNext = document.getElementById('buttonNext')
+let buttonPrev = document.getElementById('buttonPrev')
+let carrinhoCompras = document.getElementById('carrinhoCompras')
+let containershopping = document.getElementById('containershopping')
+let fecharbotao = document.getElementById('fecharbotao')
 let contador = 0
+let cardTamanho = card.length - 1
 buttonNext.addEventListener('click', () => {
-    if (contador < card.length - 1){
+    if (contador < cardTamanho){
         card[contador].style.display = 'none'
         contador ++
         card[contador].style.display = 'block'
@@ -12,4 +17,26 @@ buttonNext.addEventListener('click', () => {
         contador = 0
         card[contador].style.display = 'block'
     }
+})
+
+buttonPrev.addEventListener('click', () => {
+    if (contador != 0){
+        card[contador].style.display = 'none'
+        contador --
+        card[contador].style.display = 'block'
+    }else {
+        card[contador].style.display = 'none'
+        contador = cardTamanho
+        card[contador].style.display = 'block'
+
+    }
+})
+
+carrinhoCompras.addEventListener('click', () => {
+    containershopping.style.display = 'block'
+})
+
+fecharbotao.addEventListener('click', () => {
+    containershopping.style.display = 'none'
+
 })
